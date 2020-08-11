ea6411d351983058c3958738e4c7561e06b3f5c8
jongmin <ace4463@gmail.com>
2020-08-11 14:27:17 +0900

    cli template

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const router = express.Router();
+
+router.get = ('/', (req, res, next) => {
+    try{
+        res.send('OK');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
--- template.js
+++ template.js
@@ -1,10 +1,11 @@
-#!usr/bin/env node
+#!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
 
 const type = process.argv[2];
 const name = process.argv[3];
 const directory = process.argv[4] || '.';
+
 const htmlTemplate = `
 <!DOCTYPE html>
 <html>
@@ -35,8 +36,55 @@ router.get = ('/', (req, res, next) => {
 
 const exist = (dir) => { // 폴더 존재 확인 함수
     try{
-     // fs.accessSync(dir, fs.)
-    }catch{
-
+     fs.accessSync(dir, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+     return true;
+    }catch (e){
+        return false;
     }
-}
+};
+
+const mkdirp = (dir) => {//경로 생성 함수
+    const dirname = path
+        .relative('.', path.normalize(dir))
+        .split(path.sep)
+        .filter(p => !!p);
+    dirname.forEach((d, idx) => {
+        const pathBuilder = dirname.slice(0, idx + 1).join(path.sep);
+        if (!exist(pathBuilder)) {
+            fs.mkdirSync(pathBuilder);
+        }
+    });
+};
+
+const makeTemplate = () => { //템플릿 생성함수
+    mkdirp(directory);
+    if (type === 'html') {
+        const pathToFile = path.join(directory, `${name}.html`);
+        if (exist(pathToFile)) {
+            console.error('이미 해당파일이 존재합니다.');
+        } else {
+            fs.writeFileSync(pathToFile, htmlTemplate);
+            console.log(pathToFile,'생성 완료');
+        }
+    } else if (type === 'express-router') {
+        const pathToFile = path.join(directory, `${name}.js`);
+        if (exist(pathToFile)) {
+            console.error('이미 해당 파일이 존재합니다.');
+        } else {
+            fs.writeFileSync(pathToFile, routerTemplate);
+            console.log(pathToFile, '생성완료');
+        }
+    } else {
+        console.error('html 또는 express-router 둘 중 하나를 입력하세요.');
+    }
+};
+
+const program = () => {
+    if (!type || !name) {
+        console.error('사용방법: cli html|express-router 파일명 [생성 경로]');
+    } else {
+        makeTemplate();
+    }
+};
+
+program();
