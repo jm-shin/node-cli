8cb0643c51bc7d7dc4867e1444b3a0e7af7f9a70
jongmin <ace4463@gmail.com>
2020-08-11 15:28:03 +0900

    command.js 수정

--- command.js
+++ command.js
@@ -1,5 +1,82 @@
 #!/usr/bin/env node
 const { program } = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const htmlTemplate = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Template</title>
+    </head>
+    <body>
+    <h1>Hello</h1>
+    <p>CLI</p>
+    </body>
+</html>
+`;
+
+const routerTemplate = `
+const express = require('express');
+const router = express.Router();
+
+router.get = ('/', (req, res, next) => {
+    try{
+        res.send('OK');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+module.exports = router;
+`;
+
+const exist = (dir) => {
+    try{
+        fs.accessSync(dir, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+        return true;
+    }catch (e){
+        return false;
+    }
+};
+
+const mkdirp = (dir) => {
+    const dirname = path
+        .relative('.', path.normalize(dir))
+        .split(path.sep)
+        .filter(p => !!p);
+    dirname.forEach((d, idx) => {
+        const pathBuilder = dirname.slice(0, idx + 1).join(path.sep);
+        if (!exist(pathBuilder)) {
+            fs.mkdirSync(pathBuilder);
+        }
+    });
+};
+
+const makeTemplate = (type, name, directory) => { //템플릿 생성함수
+    mkdirp(directory);
+    if (type === 'html') {
+        const pathToFile = path.join(directory, `${name}.html`);
+        if (exist(pathToFile)) {
+            console.error('이미 해당파일이 존재합니다.');
+        } else {
+            fs.writeFileSync(pathToFile, htmlTemplate);
+            console.log(pathToFile,'생성 완료');
+        }
+    } else if (type === 'express-router') {
+        const pathToFile = path.join(directory, `${name}.js`);
+        if (exist(pathToFile)) {
+            console.error('이미 해당 파일이 존재합니다.');
+        } else {
+            fs.writeFileSync(pathToFile, routerTemplate);
+            console.log(pathToFile, '생성완료');
+        }
+    } else {
+        console.error('html 또는 express-router 둘 중 하나를 입력하세요.');
+    }
+};
 
 program
     .version('0.0.1', '-v, --version')
@@ -13,7 +90,7 @@ program
     .option('-f --filename [filename]', '파일명을 입력하세요.', 'index')
     .option('-d --directory [path]', '생성 경로를 입력하세요', '.')
     .action((type, options) => {
-        console.log(type, options.filename, options.directory);
+       makeTemplate(type, options.filename, options.directory);
     });
 
 program
